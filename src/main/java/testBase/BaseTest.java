32b74937c87927cb445737e53e63149778cca0e3
sagarwaman <sagarw094@gmail.com>
2025-10-14 22:09:13 +0530

    First commit

---
+++ src/main/java/testBase/BaseTest.java
@@ -0,0 +1,11 @@
+package testBase;
+
+public class BaseTest {
+	
+	
+	public static int addition(int a,int b) {
+		int c = a+b;
+		return c;
+	}
+
+}
---
+++ src/test/java/testPackage/DemoTest.java
@@ -0,0 +1,15 @@
+package testPackage;
+
+import testBase.BaseTest;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int c = BaseTest.addition(10, 20);
+		System.out.println(c);
+
+	}
+
+}
